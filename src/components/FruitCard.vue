ed168549ab660637489f767ccfa6661f7d58c8be
Akshansh Sourabh <78357333+SourHask@users.noreply.github.com>
2023-02-24 23:40:43 +0530

    FruitCard added

---
+++ src/components/FruitCard.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div>
+        <div style="float:right">Rs. {{item.price}}/-</div>
+        <h1>{{item.name}}</h1>
+        <h5>{{item.id}}</h5>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['item']    
+    }
+</script>
+<style scoped>
+h1{
+    font-size: 16px;
+}
+h5{
+    font-size: 12px;
+}
+</style>
