4460179ecb146d6715e3ab5a83b1e93285049fe7
Akshansh Sourabh <78357333+SourHask@users.noreply.github.com>
2023-02-24 23:47:26 +0530

    Trial Tested ButtonCounter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <button-counter>
+      <button-counter></button-counter>
+      <button-counter></button-counter>
 
-    </button-counter>
   </div>
 </template>
 
 <script>
+import ButtonCounter from '@/components/ButtonCounter.vue';
+
   export default {
-    components: {
-      
-    },
-    
-  }
+    components: { ButtonCounter }
+}
 </script>
 
 <style lang="scss" scoped>
