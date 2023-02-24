66972c169083d3b772d5f54f8523ba8c5bb03cbf
Akshansh Sourabh <78357333+SourHask@users.noreply.github.com>
2023-02-24 23:41:31 +0530

    Events added

---
+++ src/components/EditItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="number" v-model="id">
+        <input type="text" v-model="name">
+        <input type="number" v-model="price">
+        <button @click="saveItem()">Save</button>
+        <button>Cancel</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: 0,
+                name: "",
+                price: 0
+            }
+        },
+        methods: {
+            saveItem() {
+                console.log("Sending Event")
+                this.$emit('save', {"id":this.id, "name":this.name, "price":this.price})
+            }
+        },
+    }
+</script>
+<style scoped>
+</style>
