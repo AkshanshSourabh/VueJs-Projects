d5504113db4318612dd5a5ae626bd64214c53c56
Akshansh Sourabh <78357333+SourHask@users.noreply.github.com>
2023-02-24 23:42:26 +0530

    Text Box added

---
+++ src/components/MyTextBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="textValue" @change="validate()">
+        <label class="hint-text" v-if="hint">{{hint}}</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                textValue: null,
+                hint:""
+            }
+        },
+        methods:{
+            validate(){
+                if(this.textValue.length <= 0){
+                    this.hint="Value cannot be empty"
+                }else{
+                    this.$emit('input', this.textValue)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.hint-text{
+    color: red;
+    font-size: 10px;
+}
+</style>
